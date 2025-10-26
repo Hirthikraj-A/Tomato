e3321c2306b0c501bed2369422b4effc164bb76b
HirthikRaj <a.hirthikraj@gmail.com>
2025-10-26 11:59:35 +0530

    modified in backend

--- backend/config/db.js
+++ backend/config/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose";
 const connectDB = async () => {
   try {
-    await mongoose.connect("mongodb://localhost:27017/E-com"); // or process.env.MONGO_URL
+    await mongoose.connect("mongodb+srv://ahirthikraj:<Abirami1*>@cluster0.jshwx.mongodb.net/<your_db_name>?retryWrites=true&w=majority"); // or process.env.MONGO_URL
     console.log("✅ Database Connected");
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error.message);
